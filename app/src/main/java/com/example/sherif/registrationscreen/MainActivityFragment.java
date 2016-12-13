d10c008212e1e38d1c4afa65aafe85ae5b429d1c
sherifhusseineid <sherif.h.eid@gmail.com>
2016-12-13 17:41:33 +0200

    The First commit

---
+++ app/src/main/java/com/example/sherif/registrationscreen/MainActivityFragment.java
@@ -0,0 +1,29 @@
+package com.example.sherif.registrationscreen;
+
+import android.app.DatePickerDialog;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment{
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+
+}
