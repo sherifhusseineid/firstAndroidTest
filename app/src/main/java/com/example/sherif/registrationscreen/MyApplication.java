96fa688b058bdf0e2161caa97fda7f830a0fc4b6
sherifhusseineid <sherif.h.eid@gmail.com>
2017-01-24 13:16:34 +0200

    ActivityMaps

---
+++ app/src/main/java/com/example/sherif/registrationscreen/MyApplication.java
@@ -0,0 +1,20 @@
+package com.example.sherif.registrationscreen;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+/**
+ * Created by Sherif on 17/01/2017.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder().build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+    }
+}
